7b5287fb99d898d63b6edb6e5586052db8e00223
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2023-03-05 15:42:11 +0530

    C1: Navbar

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Logo() {
+    return <img src={require('../../assets/logo.png')} alt="logo" width={67} />;
+}
+
+export default Logo;
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../Logo/Logo';
+import styles from './Navbar.module.css';
+
+function Navbar() {
+    return (
+        <>
+            <div className={styles['navbar']}>
+                <Logo />
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
