58e8b9ddcc6f6fcf98c70d54d077e0ea43f5b7ba
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2023-03-05 16:15:54 +0530

    Navbar css changes

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
+import SearchBar from '../SearchBar/SearchBar';
 import styles from './Navbar.module.css';
 
 function Navbar() {
@@ -7,6 +9,8 @@ function Navbar() {
         <>
             <div className={styles['navbar']}>
                 <Logo />
+                <SearchBar />
+                <Button btnText="give feedback" />
             </div>
         </>
     );
